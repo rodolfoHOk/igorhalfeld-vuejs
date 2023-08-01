8d3358786742da4ffe7376f867f0161bd1e3a651
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-08-01 13:34:59 -0300

    initial screen part 1

---
+++ dashboard/src/views/Home/CustomHeader.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>Custom header</h1>
+</template>
+
+<script>
+export default {};
+</script>
--- dashboard/src/views/Home/index.vue
+++ dashboard/src/views/Home/index.vue
@@ -1,10 +1,37 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <div>
-    <h1 class="text-brand-main font-bold text-lg">Home</h1>
+  <custom-header @create-account="handleAccountCreate" @login="handleLogin" />
+  <contact />
+  <div class="flex justify-center py-10 bg-brand-gray">
+    <p class="font-medium text-center text-gray-800">feedbacker © 2023</p>
   </div>
 </template>
 
 <script>
-export default {};
+import { onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+import CustomHeader from './CustomHeader.vue';
+import Contact from './Contact.vue';
+
+export default {
+  components: { CustomHeader, Contact },
+  setup() {
+    const router = useRouter();
+
+    onMounted(() => {
+      const token = localStorage.getItem('token');
+      if (token) {
+        router.push({ name: 'Feedbacks' });
+      }
+    });
+
+    function handleLogin() {}
+    function handleAccountCreate() {}
+
+    return {
+      handleLogin,
+      handleAccountCreate,
+    };
+  },
+};
 </script>
