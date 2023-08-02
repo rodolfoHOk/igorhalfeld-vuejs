fcdd1ebfcfb28773dadd3ca3abd35e38d0b28a62
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-08-01 21:32:20 -0300

    login model part 1

--- dashboard/src/components/LoginModal/LoginModal.vue
+++ dashboard/src/components/LoginModal/LoginModal.vue
@@ -1,9 +1,96 @@
 <template>
   <div>
-    <h1>Modal do login</h1>
+    <div class="flex justify-between">
+      <h1 class="text-4xl font-black text-gray-800">Entre na sua conta</h1>
+
+      <button class="text-4xl text-gray-600 focus:outline-none" @click="close">
+        &times;
+      </button>
+    </div>
+
+    <div class="mt-16">
+      <form @submit.prevent="handleSubmit">
+        <label class="block">
+          <span class="text-lg font-medium text-gray-800">E-mail</span>
+
+          <input
+            type="email"
+            class="block w-full px-4 py-3 mt-1 text-lg bg-green-100 border-2 border-transparent rounded"
+            :class="{ 'border-brand-danger': !!state.email.errorMessage }"
+            placeholder="jane.doe@email.com"
+            v-model="state.email.value"
+          />
+
+          <span
+            v-if="!!state.email.errorMessage"
+            class="block font-medium text-brand-danger"
+          >
+            {{ state.email.errorMessage }}
+          </span>
+        </label>
+
+        <label class="block mt-10">
+          <span class="text-lg font-medium text-gray-800">Senha</span>
+
+          <input
+            type="password"
+            class="block w-full px-4 py-3 mt-1 text-lg bg-green-100 border-2 border-transparent rounded"
+            :class="{ 'border-brand-danger': !!state.password.errorMessage }"
+            placeholder="******"
+            v-model="state.password.value"
+          />
+
+          <span
+            v-if="!!state.password.errorMessage"
+            class="block font-medium text-brand-danger"
+          >
+            {{ state.password.errorMessage }}
+          </span>
+        </label>
+
+        <button
+          type="submit"
+          class="px-8 py-3 mt-10 text-2xl font-bold text-white rounded-full bg-brand-main focus:outline-none transition-all duration-150 hover:bg-brand-main/90"
+          :class="{ 'opacity-50': state.isLoading }"
+          :disabled="state.isLoading"
+        >
+          Entrar
+        </button>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import { reactive } from 'vue';
+import useModal from '@/hooks/useModal';
+
+export default {
+  setup() {
+    const modal = useModal();
+
+    const state = reactive({
+      hasError: false,
+      isLoading: false,
+      email: {
+        value: '',
+        errorMessage: '',
+      },
+      password: {
+        value: '',
+        errorMessage: '',
+      },
+    });
+
+    function handleSubmit() {
+      // to do
+    }
+
+    return {
+      state,
+      close: modal.close,
+      handleSubmit,
+    };
+  },
+};
 </script>
