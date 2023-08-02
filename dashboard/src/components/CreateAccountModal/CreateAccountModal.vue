78cee8aeda69838a04d8134c1c9239d6d68c7314
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-08-01 23:31:44 -0300

    create account modal

--- dashboard/src/components/CreateAccountModal/CreateAccountModal.vue
+++ dashboard/src/components/CreateAccountModal/CreateAccountModal.vue
@@ -1,9 +1,200 @@
 <template>
   <div>
-    <h1>Modal de criação de conta</h1>
+    <div class="flex justify-between">
+      <h1 class="text-4xl font-black text-gray-800">Crie uma conta</h1>
+
+      <button class="text-4xl text-gray-600 focus:outline-none" @click="close">
+        &times;
+      </button>
+    </div>
+
+    <div class="mt-16">
+      <form @submit.prevent="handleSubmit">
+        <label class="block">
+          <span class="text-lg font-medium text-gray-800">Nome</span>
+
+          <input
+            type="text"
+            class="block w-full px-4 py-3 mt-1 text-lg bg-gray-100 border-2 border-transparent rounded"
+            :class="{
+              'border-transparent': !state.name.errorMessage,
+              'border-brand-danger': !!state.name.errorMessage,
+            }"
+            placeholder="Jane Doe"
+            v-model="state.name.value"
+          />
+
+          <span
+            v-if="!!state.name.errorMessage"
+            class="block font-medium text-brand-danger mt-1"
+          >
+            {{ state.name.errorMessage }}
+          </span>
+        </label>
+
+        <label class="block mt-10">
+          <span class="text-lg font-medium text-gray-800">E-mail</span>
+
+          <input
+            type="email"
+            class="block w-full px-4 py-3 mt-1 text-lg bg-gray-100 border-2 border-transparent rounded"
+            :class="{
+              'border-transparent': !state.email.errorMessage,
+              'border-brand-danger': !!state.email.errorMessage,
+            }"
+            placeholder="jane.doe@email.com"
+            v-model="state.email.value"
+          />
+
+          <span
+            v-if="!!state.email.errorMessage"
+            class="block font-medium text-brand-danger mt-1"
+          >
+            {{ state.email.errorMessage }}
+          </span>
+        </label>
+
+        <label class="block mt-10">
+          <span class="text-lg font-medium text-gray-800">Senha</span>
+
+          <input
+            type="password"
+            class="block w-full px-4 py-3 mt-1 text-lg bg-gray-100 border-2 rounded"
+            :class="{
+              'border-transparent': !state.password.errorMessage,
+              'border-brand-danger': !!state.password.errorMessage,
+            }"
+            placeholder="******"
+            v-model="state.password.value"
+          />
+
+          <span
+            v-if="!!state.password.errorMessage"
+            class="block font-medium text-brand-danger mt-1"
+          >
+            {{ state.password.errorMessage }}
+          </span>
+        </label>
+
+        <button
+          type="submit"
+          class="px-8 py-3 mt-10 text-2xl font-bold text-white rounded-full bg-brand-main focus:outline-none transition-all duration-150 hover:bg-brand-main/90"
+          :class="{ 'opacity-50': state.isLoading }"
+          :disabled="state.isLoading"
+        >
+          <icon
+            v-if="state.isLoading"
+            name="LoadingIcon"
+            class="animate-spin"
+          />
+          <span v-else>Criar conta</span>
+        </button>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import { reactive } from 'vue';
+import { useRouter } from 'vue-router';
+import { useField } from 'vee-validate';
+import { useToast } from 'vue-toastification';
+import useModal from '@/hooks/useModal';
+import Icon from '../Icon/IconFactory.vue';
+import {
+  validateEmptyAndLength3,
+  validateEmptyAndEmail,
+} from '../../utils/validators';
+import services from '../../services';
+
+export default {
+  components: { Icon },
+  setup() {
+    const router = useRouter();
+    const toast = useToast();
+    const modal = useModal();
+
+    const { value: nameValue, errorMessage: nameErrorMessage } = useField(
+      'name',
+      validateEmptyAndLength3
+    );
+
+    const { value: emailValue, errorMessage: emailErrorMessage } = useField(
+      'email',
+      validateEmptyAndEmail
+    );
+
+    const { value: passwordValue, errorMessage: passwordErrorMessage } =
+      useField('password', validateEmptyAndLength3);
+
+    const state = reactive({
+      hasError: false,
+      isLoading: false,
+      name: {
+        value: nameValue,
+        errorMessage: nameErrorMessage,
+      },
+      email: {
+        value: emailValue,
+        errorMessage: emailErrorMessage,
+      },
+      password: {
+        value: passwordValue,
+        errorMessage: passwordErrorMessage,
+      },
+    });
+
+    async function login({ email, password }) {
+      const { data, errors } = await services.auth.login({
+        email,
+        password,
+      });
+
+      if (!errors) {
+        window.localStorage.setItem('token', data.token);
+        router.push({ name: 'Feedbacks' });
+        modal.close();
+      }
+
+      state.isLoading = false;
+    }
+
+    async function handleSubmit() {
+      try {
+        toast.clear();
+        state.isLoading = true;
+        const { errors } = await services.auth.register({
+          name: state.name.value,
+          email: state.email.value,
+          password: state.password.value,
+        });
+
+        if (!errors) {
+          await login({
+            email: state.email.value,
+            password: state.password.value,
+          });
+
+          return;
+        }
+
+        if (errors.status === 400) {
+          toast.error('Ocorreu um erro ao criar a conta');
+        }
+
+        state.isLoading = false;
+      } catch (error) {
+        state.isLoading = false;
+        state.hasError = !!error;
+        toast.error('Ocorreu um erro ao criar a conta');
+      }
+    }
+
+    return {
+      state,
+      close: modal.close,
+      handleSubmit,
+    };
+  },
+};
 </script>
