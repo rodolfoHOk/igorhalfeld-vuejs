ce0d79b1562993f45e2b7b7fb6eff0a2fdcf541a
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-08-03 13:59:29 -0300

    credentials screen part 1

--- dashboard/src/views/Credentials/CredentialsIndex.vue
+++ dashboard/src/views/Credentials/CredentialsIndex.vue
@@ -1,9 +1,23 @@
 <template>
   <div>
-    <h1>Credentials</h1>
+    <div class="flex justify-center w-full h-28 bg-brand-main">
+      <header-logged />
+    </div>
+
+    <div class="flex flex-col items-center justify-center h-64 bg-brand-gray">
+      <h1 class="text-4xl font-black text-center text-gray-800">Credenciais</h1>
+
+      <p class="mt-2 text-2xl font-regular text-center text-gray-800">
+        Guia de instalação e geração de suas credenciais
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import HeaderLogged from '../../components/HeaderLogged/HeaderLogged.vue';
+
+export default {
+  components: { HeaderLogged },
+};
 </script>
--- dashboard/src/views/Feedbacks/FeedbacksIndex.vue
+++ dashboard/src/views/Feedbacks/FeedbacksIndex.vue
@@ -1,9 +1,17 @@
 <template>
   <div>
+    <div class="flex justify-center w-full h-28 bg-brand-main">
+      <header-logged />
+    </div>
+
     <h1>Feedbacks</h1>
   </div>
 </template>
 
 <script>
-export default {};
+import HeaderLogged from '../../components/HeaderLogged/HeaderLogged.vue';
+
+export default {
+  components: { HeaderLogged },
+};
 </script>
