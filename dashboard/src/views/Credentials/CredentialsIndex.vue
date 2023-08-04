7a78d37539070d22fe27cc4dabeef9ab3687f199
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-08-03 21:30:35 -0300

    credentials screen part 3

--- dashboard/src/views/Credentials/CredentialsIndex.vue
+++ dashboard/src/views/Credentials/CredentialsIndex.vue
@@ -22,8 +22,16 @@
           Este aqui é a sua chave de api
         </p>
 
+        <content-loader
+          v-if="store.Global.isLoading || state.isLoading"
+          class="rounded"
+          width="600px"
+          height="50px"
+        />
+
         <div
-          class="flex py-3 px-5 mt-2 rounded items-center justify-between bg-brand-gray w-[630px] text-lg font-medium"
+          v-else
+          class="flex py-3 px-5 mt-2 rounded items-center justify-between bg-brand-gray w-full lg:w-[630px] text-lg font-medium"
         >
           <span>{{ store.User.currentUser.apiKey }}</span>
 
@@ -48,8 +56,16 @@
           Coloque o script abaixo no seu site para começar a receber feedbacks
         </p>
 
+        <content-loader
+          v-if="store.Global.isLoading || state.isLoading"
+          class="rounded"
+          width="600px"
+          height="50px"
+        />
+
         <div
-          class="py-3 px-5 mt-2 rounded bg-brand-gray w-[630px] text-lg font-medium overflow-x-scroll"
+          v-else
+          class="py-3 px-5 mt-2 rounded bg-brand-gray w-full lg:w-[630px] text-lg font-medium overflow-x-scroll"
         >
           <pre>
 &lt;script src="https://rodolfoHOk-feedbacker-widget.netlify.app?api_key={{
@@ -67,15 +83,22 @@ import useStore from '@/hooks/useStore';
 import HeaderLogged from '../../components/HeaderLogged/HeaderLogged.vue';
 import palette from '../../../palette';
 import Icon from '../../components/Icon/IconFactory.vue';
+import ContentLoader from '../../components/ContentLoader/ContentLoader.vue';
+import { reactive } from 'vue';
 
 export default {
-  components: { HeaderLogged, Icon },
+  components: { HeaderLogged, Icon, ContentLoader },
 
   setup() {
     const store = useStore();
 
+    const state = reactive({
+      isLoading: false,
+    });
+
     return {
       store,
+      state,
       brandColors: palette.brand,
     };
   },
